12ba4a1cb16efb43ebcab5e912c7d586aad112e4
Alessandro Morelli <alessandro.morelli@snyffo.com>
2020-07-17 11:58:11 +0200

    Ignore missing composer.json

--- src/ConfigCollectionFactory.php
+++ src/ConfigCollectionFactory.php
@@ -1,68 +1,74 @@
 <?php
 
-declare(strict_types=1);
+declare( strict_types=1 );
 
 namespace TypistTech\Imposter;
 
-class ConfigCollectionFactory
-{
-    public static function forProject(
-        ProjectConfigInterface $projectConfig,
-        Filesystem $filesystem
-    ): ConfigCollectionInterface {
-        return static::addRequiredPackageConfigsRecursively(
-            new ConfigCollection(),
-            $projectConfig,
-            $projectConfig,
-            $filesystem
-        );
-    }
+class ConfigCollectionFactory {
+	public static function forProject(
+		ProjectConfigInterface $projectConfig,
+		Filesystem $filesystem
+	): ConfigCollectionInterface {
+		return static::addRequiredPackageConfigsRecursively(
+			new ConfigCollection(),
+			$projectConfig,
+			$projectConfig,
+			$filesystem
+		);
+	}
 
-    private static function addRequiredPackageConfigsRecursively(
-        ConfigCollectionInterface $configCollection,
-        ProjectConfigInterface $projectConfig,
-        ConfigInterface $config,
-        Filesystem $filesystem
-    ): ConfigCollectionInterface {
-        $filteredRequires = static::getFilteredPackages($projectConfig, $config);
+	private static function addRequiredPackageConfigsRecursively(
+		ConfigCollectionInterface $configCollection,
+		ProjectConfigInterface $projectConfig,
+		ConfigInterface $config,
+		Filesystem $filesystem
+	): ConfigCollectionInterface {
+		$filteredRequires = static::getFilteredPackages( $projectConfig, $config );
 
-        foreach ($filteredRequires as $package) {
-            $packageConfig = ConfigFactory::build(
-                $projectConfig->getVendorDir() . $package . '/composer.json',
-                $filesystem
-            );
+		foreach ( $filteredRequires as $package ) {
 
-            $configCollection->add($packageConfig);
+			$path = $projectConfig->getVendorDir() . $package . '/composer.json';
+			if ( $filesystem->isFile( $path ) ) {
 
-            static::addRequiredPackageConfigsRecursively(
-                $configCollection,
-                $projectConfig,
-                $packageConfig,
-                $filesystem
-            );
-        }
+				$packageConfig = ConfigFactory::build(
+					$path,
+					$filesystem
+				);
 
-        return $configCollection;
-    }
+				$configCollection->add( $packageConfig );
 
-    /**
-     * @param ProjectConfigInterface $projectConfig
-     * @param ConfigInterface        $config
-     *
-     * @return string[]
-     */
-    private static function getFilteredPackages(ProjectConfigInterface $projectConfig, ConfigInterface $config): array
-    {
-        $requiredPackages = array_filter($config->getRequires(), function (string $package) {
-            return (false !== strpos($package, '/'));
-        });
+				static::addRequiredPackageConfigsRecursively(
+					$configCollection,
+					$projectConfig,
+					$packageConfig,
+					$filesystem
+				);
+			}
+		}
 
-        $nonComposerPackages = array_filter($requiredPackages, function (string $package) {
-            return (false === strpos($package, 'composer/'));
-        });
+		return $configCollection;
+	}
 
-        return array_filter($nonComposerPackages, function (string $package) use ($projectConfig) {
-            return ! in_array($package, $projectConfig->getExcludes(), true);
-        });
-    }
+	/**
+	 * @param ProjectConfigInterface $projectConfig
+	 * @param ConfigInterface $config
+	 *
+	 * @return string[]
+	 */
+	private static function getFilteredPackages(
+		ProjectConfigInterface $projectConfig,
+		ConfigInterface $config
+	): array {
+		$requiredPackages = array_filter( $config->getRequires(), function ( string $package ) {
+			return ( false !== strpos( $package, '/' ) );
+		} );
+
+		$nonComposerPackages = array_filter( $requiredPackages, function ( string $package ) {
+			return ( false === strpos( $package, 'composer/' ) );
+		} );
+
+		return array_filter( $nonComposerPackages, function ( string $package ) use ( $projectConfig ) {
+			return ! in_array( $package, $projectConfig->getExcludes(), true );
+		} );
+	}
 }
